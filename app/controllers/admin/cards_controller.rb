b0071e2690d12c6322c9665fc737a0d4599442a6
kyuuki <fdear.kobayashi@nifty.com>
2018-02-06 11:33:51 +0000

    カード追加、編集、削除機能追加

--- app/controllers/admin/cards_controller.rb
+++ app/controllers/admin/cards_controller.rb
@@ -2,4 +2,55 @@ class Admin::CardsController < Admin::ApplicationController
   def index
     @cards = Card.all
   end
+
+  def new
+    @card = Card.new
+  end
+
+  def edit
+    @card = Card.find(params[:id])
+  end
+
+  def create
+    @card = Card.new(card_params)
+
+    respond_to do |format|
+      if @card.save
+        format.html { redirect_to admin_cards_url, notice: 'Card was successfully created.' }
+        format.json { render :show, status: :created, location: @card }
+      else
+        format.html { render :new }
+        format.json { render json: @card.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def update
+    @card = Card.find(params[:id])
+
+    respond_to do |format|
+      if @card.update(card_params)
+        format.html { redirect_to admin_cards_url, notice: 'Card was successfully updated.' }
+        format.json { render :show, status: :ok, location: @card }
+      else
+        format.html { render :edit }
+        format.json { render json: @card.errors, status: :unprocessable_entity }
+      end
+    end
+  end
+
+  def destroy
+    @card = Card.find(params[:id])
+
+    @card.destroy
+    respond_to do |format|
+      format.html { redirect_to admin_cards_url, notice: 'Card was successfully destroyed.' }
+      format.json { head :no_content }
+    end
+  end
+
+  private
+    def card_params
+      params.require(:card).permit(:question, :answer, :comment)
+    end
 end
