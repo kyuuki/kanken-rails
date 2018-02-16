c813d7717296e6c387becb573bd6ae5e86d43865
kyuuki <fdear.kobayashi@nifty.com>
2018-02-16 15:54:11 +0000

    正答率を表示

--- app/models/card.rb
+++ app/models/card.rb
@@ -1,3 +1,23 @@
 class Card < ApplicationRecord
   has_many :log_actions
+
+  def count_action_ok
+    # 1 はなんとかせな
+    self.log_actions.where(action: LogAction::ACTION_OK).count
+  end
+
+  def count_action_ng
+    self.log_actions.where(action: LogAction::ACTION_NG).count
+  end
+
+  # 正答率
+  def correct_answer_rate
+    count_action_ok = self.count_action_ok
+    count_action_ng = self.count_action_ng
+
+    return 0 if (count_action_ok + count_action_ng) == 0
+
+    # どっちがか float なら結果も float
+    return count_action_ok / (count_action_ok + count_action_ng).to_f
+  end
 end
--- app/models/log_action.rb
+++ app/models/log_action.rb
@@ -1,4 +1,7 @@
 class LogAction < ApplicationRecord
   belongs_to :client
   belongs_to :card
+
+  ACTION_NG = 1
+  ACTION_OK = 2
 end
