336b38719a7a87f346bf123b2923fa0cae9b1c29
kyuuki <fdear.kobayashi@nifty.com>
2018-05-04 10:37:24 +0000

    問題を作成月で集計できるようにする

--- app/models/admin_user.rb
+++ app/models/admin_user.rb
@@ -5,4 +5,10 @@ class AdminUser < ApplicationRecord
          :recoverable, :rememberable, :trackable, :validatable
 
   has_many :card_owners
+
+  # XXXX 年 XX 月に作成したカード数
+  def count_card_owners_created_in_year_month(year, month)
+    date = Date.new(year, month, 1)
+    self.card_owners.where(created_at: [date.beginning_of_month.beginning_of_day..date.end_of_month.end_of_day]).count
+  end
 end
