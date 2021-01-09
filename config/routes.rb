1abfcbc7f6d32c42b151bc207223c434c64c4b64
kyuuki <kyuuki.japan@gmail.com>
2021-01-09 09:07:02 +0000

    Devise でログイン機能追加

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,12 @@ Rails.application.routes.draw do
     end
   end
 
+  devise_for :users,
+             controllers: {
+               sessions: 'users/sessions',
+               registrations: 'users/registrations'
+             }
+
   # 管理画面
 
   # https://www.tmp1024.com/programming/post-4661
