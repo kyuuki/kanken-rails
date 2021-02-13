c89765252f78cb513503a49f5893efa324f8f2bd
kyuuki <kyuuki.japan@gmail.com>
2021-02-13 05:12:42 +0000

    SendGrid 対応

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -68,6 +68,16 @@ Rails.application.configure do
   # config.action_mailer.raise_delivery_errors = false
 
   config.action_mailer.default_url_options = { host: 'kanken.akoba.xyz', protocol: "https" }
+  
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => 'kanken.akoba.xyz',
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
 
   # Enable locale fallbacks for I18n (makes lookups for any locale fall back to
   # the I18n.default_locale when a translation cannot be found).
