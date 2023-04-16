2bb43f635e69c133530aaac9c468ac6bb0ce4905
kyuuki <kyuuki.japan@gmail.com>
2023-04-16 09:54:36 +0000

    Rails バージョンアップで消えてしまっていたメール関係の設定を追加

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -41,6 +41,15 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {
+    host: Rails.configuration.setting.host,
+    port: Rails.configuration.setting.port
+  }
+
+  # https://github.com/ryanb/letter_opener
+  config.action_mailer.delivery_method = :letter_opener
+  config.action_mailer.perform_deliveries = true
+
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,21 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
 
+  config.action_mailer.default_url_options = {
+    host: Rails.configuration.setting.host,
+    protocol: "https"
+  }
+
+  config.action_mailer.smtp_settings = {
+    :user_name => 'apikey',
+    :password => ENV['SENDGRID_API_KEY'],
+    :domain => config.host,
+    :address => 'smtp.sendgrid.net',
+    :port => 587,
+    :authentication => :plain,
+    :enable_starttls_auto => true
+  }
+
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
